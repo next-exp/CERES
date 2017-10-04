10bc5253e32fc7fb21b115ab2d598437fc114c6f
jmbenlloch <info@jmbenlloch.net>
2017-10-04 14:34:04 +0800

    Dev templates

--- ceres/templates.py
+++ ceres/templates.py
@@ -5,10 +5,28 @@ from ceres import versions
 
 templates = {
     'irene' : {
-        'kr1300' : 'irene_kr_s2_1300.conf'
+        'csexample' : 'irene_CsProd_example.conf',
+        'kr1300'    : 'irene_kr_s2_1300.conf'    ,
+        'kr1300i'   : 'irene_kr_s2_1300i.conf'   ,
+        'kr1500i'   : 'irene_kr_s2_1500i.conf'   ,
+        'kr1650i'   : 'irene_kr_s2_1650i.conf'   ,
+        'kr1900i'   : 'irene_kr_s2_1900i.conf'   ,
+        'kr2200i'   : 'irene_kr_s2_2200i.conf'   ,
+        'na_s1'     : 'irene_na_s1.conf'         ,
+        'cs2000'    : 'irene_cs_s2_2000.conf'    ,
+        'th2000'    : 'irene_th_s2_2000.conf'    ,
+        'bg2000'    : 'irene_bg_s2_2000.conf'    ,
+        'test'      : 'irene_test.conf'
     },
     'dorothea' : {
-        'kr1300' : 'dorothea_kr.conf'
+        'csexample' : 'dorothea_example.conf',
+        'kr'        : 'dorothea_kr.conf',
+        'na_s1'     : 'dorothea_na.conf',
+        'cs2000'    : 'dorothea_cs.conf',
+        'th2000'    : 'dorothea_th.conf',
+    },
+    'penthesilea' : {
+        'bg2000'    : 'penthesilea_bg.conf'
     }
 }
 
