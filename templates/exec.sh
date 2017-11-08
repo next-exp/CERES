76ecebdb1999d3cd57fbc806d9dc58bd05d3e13f
icuser <icuser@frontend1next.(none)>
2017-11-09 00:57:26 +0100

    add ic bin dir to path in exec.sh

--- templates/exec.sh
+++ templates/exec.sh
@@ -11,5 +11,6 @@ export PATH="/software/miniconda3/bin:$PATH"
 export LD_LIBRARY_PATH="/software/miniconda3/lib:$LD_LIBRARY_PATH"
 export ICTDIR=/software/IC
 export ICDIR=$ICTDIR/invisible_cities
+export PATH="$ICTDIR/bin:$PATH"
 export PYTHONPATH=$ICTDIR:$PYTHONPATH
 source activate IC3.6
